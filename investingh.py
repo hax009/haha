a6aadd6452cbe13d2f677e3874914e4b50de31ea
chaoran <chaoran@mycarauction.com>
2022-04-12 16:50:16 -0700

    Completed est_pdf() and better formatted the combination method.

--- investingh.py
+++ investingh.py
@@ -1,71 +1,38 @@
 import numpy as np
 from itertools import combinations
 
+def get_index(n, k):
+    """
+    Helper function to get all combinations
+    :param n: Objects
+    :param k: Sample(s)
+    :return:
+    """
+    return combinations([i for i in range(n)], k)
 
-def get_index(i):
-    comb = combinations(p, i)
-
-    # Print the obtained combinations
-    comblist = []
-    for k in list(comb):
-        comblist.append(k)
-        
-    abk = []
-    for ii in comblist:
-        dd = [0]*len(p)
-        for ik in range(len(p)):
-            if p[ik] in ii:
-                dd[ik] = 1
-        abk.append(dd)
-    return abk
-
-
-
-
-
-
-
-
-
-def est_pdf(p):
-    result = np.empty(len(p))
-    #def get_index(i):
-    
-    for i in range(len(p)):
-        indexes = get_index(i)
-        
+def est_pdf(p: list):
+    """
+    Calculate the est_pdf
+    :param p: list of probabilities of investing success
+    :return: est_pdf
+    """
+    result = np.zeros(len(p))
+    for k in range(len(p)):
+        if k == 0:
+            company_combination_prob = np.array(p)
+            company_combination_prob = 1 - company_combination_prob
+            result[k] = company_combination_prob.prod()
+            continue
+        company_combinations = get_index(len(p), k)
         #p = [0.4,0.5,0.1]
         #ex: i = 2, return indexes = [[1,1,0],[0,1,1],[1,0,1]]
-        
-        for j in range(len(indexes)):
-            #p_k = 0
-            #j = 1
-            p_k = []
-            
-            for k in indexes[j]:
-                sm = []
-                cc = 0
-                #k = 1
-                #p_k += p[indexes[j][k]==1]*(1-p[indexes[j][k]==0])
-                if p[cc] == k:
-                    sm.append(p[cc])
-                else:
-                    sm.append(1-p[cc])
-                cc += 1
-            p_k.append(np.prod(sm))
-            
-            result[i] = np.sum(p_k)
-                
-                
-                
-                
-                
-                
-                #for og in range(len(p)):
-                    #if k == 1:
-                        #p_k.append(p[og])
-                    #else:
-                        #p_k.append(1-p[og])
-                #p_kk = np.prod(p_k)
-            #result[i] = p_kk
+        for company_combination in company_combinations:
+                company_combination_prob = np.array(p)
+                company_combination_prob = 1 - company_combination_prob
+                company_combination_prob[list(company_combination)] = 1 - company_combination_prob[list(company_combination)]
+                result[k] += company_combination_prob.prod()
     return result
+
+if __name__ == '__main__':
+    p = [0.34537093, 0.75580255, 0.61383029, 0.06516869, 0.46175061, 0.57079639, 0.03297377, 0.88347158, 0.91001774, 0.09064271]
+    print(est_pdf(p))
